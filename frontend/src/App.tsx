2772457e189f86d1bf99a7452173fa62301ab674
ParasDevaliya <parasdevliya55@gmail.com>
2025-11-05 10:27:00 +0530

    frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-cyan-800 font-bold'> hello word </h1>
+         <Button variant="outline" className='mt-5'>Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
